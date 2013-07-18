41a7030f3a19bf2c95c2d2887fc4c9e1d8125f55
WAKAYAMA Shirou <shirou.faw@gmail.com>
2013-07-18 16:55:19 +0900

    add doc.go to show url at godoc.org.

---
+++ doc.go
@@ -0,0 +1,12 @@
+// Copyright 2013 WAKAYAMA Shirou. All rights reserved.
+// Use of this source code is governed by a MIT license
+// that can be found in the LICENSE file.
+
+/*
+Package gofaker provides fake data easily.
+
+source code and project home: https://bitbucket.org/r_rudi/gofaker/
+
+*/
+
+package gofaker
